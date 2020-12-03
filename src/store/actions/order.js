7d06dcbb36d7f02f53c51b0f804ddae9baa3219e
genakimov <ga.spb7@gmail.com>
2020-12-03 22:50:11 +0300

    fix commit

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,7 +1,7 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
-export const purchaseBurgerSuccess = (id, orderData) => {
+export const purchaseBurgerSuccess = ( id, orderData ) => {
     return {
         type: actionTypes.PURCHASE_BURGER_SUCCESS,
         orderId: id,
@@ -9,29 +9,29 @@ export const purchaseBurgerSuccess = (id, orderData) => {
     };
 };
 
-export const purchaseBurgerFail = (error) => {
+export const purchaseBurgerFail = ( error ) => {
     return {
-        type: actionTypes.FETCH_INGREDIENTS_FAILED,
+        type: actionTypes.PURCHASE_BURGER_FAIL,
         error: error
     };
-};
+}
 
 export const purchaseBurgerStart = () => {
     return {
-      type: actionTypes.PURCHASE_BURGER_START
+        type: actionTypes.PURCHASE_BURGER_START
     };
 };
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = ( orderData ) => {
     return dispatch => {
-        dispatch(purchaseBurgerStart());
-        axios.post('/orders.json', orderData)
-            .then(response => {
-                console.log(response.data);
-                dispatch(purchaseBurgerSuccess( response.data.name, orderData ));
-            })
-            .catch(error => {
-                dispatch(purchaseBurgerFail(error));
-            })
+        dispatch( purchaseBurgerStart() );
+        axios.post( '/orders.json', orderData )
+            .then( response => {
+                console.log( response.data );
+                dispatch( purchaseBurgerSuccess( response.data.name, orderData ) );
+            } )
+            .catch( error => {
+                dispatch( purchaseBurgerFail( error ) );
+            } );
     };
 };
